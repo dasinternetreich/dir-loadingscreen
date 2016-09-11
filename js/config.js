3dd9abcd098ac3ab061462ac48cb8559c9c1a892
dasinternetreich <dasinternetreich@gmail.com>
2016-09-11 15:25:13 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -97,7 +97,7 @@ var l_musicRandom = true;
 
 /*Music volume
 Choose a value between 0 and 100*/
-var l_musicVolume = 8;
+var l_musicVolume = 6;
 
 /*Enable custom messages?*/
 var l_messagesEnabled = true;
