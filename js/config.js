d284c57cddde8065a15a76ed3847942aeec8c891
dasinternetreich <dasinternetreich@gmail.com>
2017-02-03 00:10:57 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -68,19 +68,19 @@ var l_bgImagesRandom = true;
 /*(Image-background only)
 Delay between background images changes
 in milliseconds*/
-var l_bgImageDuration = 5000;
+var l_bgImageDuration = 20000;
 
 /*(Image-background only)
 Image background fade velocity
 in milliseconds*/
-var l_bgImageFadeVelocity = 2000;
+var l_bgImageFadeVelocity = 3000;
 
 /*Enable background overlay?*/
 var l_bgOverlay = true;
 
 /*Background darkening amount
 0 for none (0%), 100 for pitch black (100%)*/
-var l_bgDarkening = 40;
+var l_bgDarkening = 50;
 
 /*Music player
 'true' or 'false' to enable music in the background*/
@@ -92,12 +92,12 @@ var l_musicDisplay = true;
 /*Music playlist
 Place how much Youtube IDs/.ogg you want*/
 var l_musicPlaylist = [
-	{youtube: "F9fNOPwqa4g", name: "TS - There's A Candle Burning Bright"},
-	{youtube: "E4UxwO4kxX4", name: "TS - Angel Of No Man's Land"},
-	{youtube: "NnkynUZ3p04", name: "TS - The Generals Never Listen"},
-	{youtube: "ChD-IWPRxsA", name: "Goodbye Dolly Gray - Harry MacDonough"},
-	{youtube: "hCILkRNga6o", name: "Oh! It's a lovely war!"},
-	{youtube: "JNJ2hbisvJg", name: "SM Kaiser Wilhelm II"}
+	{youtube: "E4UxwO4kxX4", name: "TS - There's A Candle Burning Bright"},
+	{youtube: "ChD-IWPRxsA", name: "TS - The Generals Never Listen"},
+	{youtube: "F9fNOPwqa4g", name: "TS - The Sailor's Life For Me"},
+	{youtube: "ltUXJmUc8tM", name: "O du Deutschland, Ich muß Marschieren"},
+	{youtube: "sImy_hQ8Jvk", name: "HM II, 145 VDHM - Unsere Marine"}
+
 ];
 
 /*Random music order?*/
@@ -105,16 +105,24 @@ var l_musicRandom = true;
 
 /*Music volume
 Choose a value between 0 and 100*/
-var l_musicVolume = 30;
+var l_musicVolume = 10;
 
 /*Enable custom messages?*/
 var l_messagesEnabled = true;
 
 /*Enter your custom messages below*/
 var l_messages = [
-"This was a war to end all wars. -American President Woodrow Wilson",
-"As a soldier I survived World War I when most of my comrades did not. -Cpl Lester B Pearson",
-"Give me a woman who really loves beer and and I will conquer the world... -Kaiser Wilhelm II"
+"They've won the war that ended all wars. -American President Woodrow Wilson",
+"As a soldier I survived the World War when most of my comrades did not. -Cpl Lester B Pearson",
+"Give me a woman who really loves beer and and I will conquer the world... -Kaiser Wilhelm II",
+"I have a slight attraction to Panzerwagens... -Kürassier Adolf Karlheinz",
+"Break the chains! Down with the Kaiser! -British Syndicalist Roland Rorrow",
+"I always fancied Georgy but he wanted this war. -Kaiser Wilhelm II",
+"America should be first and always first. -Huey Long Leader of America First Union Party",
+"The independence of France will always be at risk if Germany is allowed to live. -French Head of State Sébastien Faure",
+"Germany will always be the pinnacle of mankind. -Feldwebel Adolf Hitler",
+"Czar Nicholaas is only fit to live in a country house and grow turnips. -Kaiser Wilhelm II",
+"But how? Oy vey! -Chaim Goldstein Commercial Bank Owner"
 
 ];
 
@@ -123,8 +131,8 @@ var l_messagesRandom = true;
 
 /*Delay between message changes
 in milliseconds*/
-var l_messagesDelay = 5000;
+var l_messagesDelay = 10000;
 
 /*Messages fade time
 in milliseconds*/
-var l_messagesFade = 1000;
+var l_messagesFade = 2000;
