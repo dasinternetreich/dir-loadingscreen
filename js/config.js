e1d214e1397d2c0a9ea7f58024b7da448577cb0e
dasinternetreich <dasinternetreich@gmail.com>
2017-02-02 22:07:42 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -50,15 +50,15 @@ var l_background = "backgrounds/videos/glimmering.webm";
 Place one or more images
 Multiple images will automatically shuffle between them*/
 var l_bgImages = [
-    "backgrounds/images/w3.png",
+    "backgrounds/images/w1.png",
+	"backgrounds/images/w2.png",
+	"backgrounds/images/w3.png",
+	"backgrounds/images/w4.png",
+	"backgrounds/images/w5.png",
+	"backgrounds/images/w6.png",
 	"backgrounds/images/w7.png",
 	"backgrounds/images/w8.png",
-	"backgrounds/images/w9.png",
-	"backgrounds/images/w5.png",
-	"backgrounds/images/w2.png",
-	"backgrounds/images/w4.png",
-	"backgrounds/images/w6.png",
-	"backgrounds/images/w1.png"
+	"backgrounds/images/w9.png"
 	
 ];
 
